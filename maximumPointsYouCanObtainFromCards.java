4e0da093629a8a613861a8fd4c0359d817adf403
Ayush Shukla <123shuklaayush@gmail.com>
2023-07-04 22:48:12 +0530

    Changed the wrong code

--- maximumPointsYouCanObtainFromCards.java
+++ maximumPointsYouCanObtainFromCards.java
@@ -5,26 +5,23 @@ import java.util.PriorityQueue;
 
 public class maximumPointsYouCanObtainFromCards {
     public static int maxScore(int[] cardPoints, int k) {
-        PriorityQueue<Integer> p = new PriorityQueue<>(Comparator.reverseOrder());
-        for(int i = 0; i<k ; i++){
-            p.add(cardPoints[i]);
+            int n = cardPoints.length;
+            int[] left = new int[n + 1];
+            int[] right = new int[n + 1];
+            for (int i = 0; i < n; i++) {
+                left[i + 1] = left[i] + cardPoints[i];
+                right[i + 1] = cardPoints[n - i - 1] + right[i];
+            }
+            int max = 0;
+            for (int i = 0; i <= k; i++) {
+                int currSum = left[i] + right[k - i];
+                max = Math.max(currSum, max);
+            }
+            return max;
         }
-        for(int i = cardPoints.length-1; i>k; i--){
-            p.add(cardPoints[i]);
-        }
-        int max = 0;
-        for(int i = 0; i< k; i++){
-            if(!p.isEmpty())
-             max+=p.peek();
-            p.poll();
-        }
-        return max;
-    }
-
     public static void main(String[] args) {
         PriorityQueue<Integer> pq= new PriorityQueue<>(Comparator.reverseOrder());
         int[] nums= {1,2,3,4,5,6,1};
-
         System.out.println(maxScore(nums, 3));
     }
 }
